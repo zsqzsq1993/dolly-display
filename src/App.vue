b46bc7f8a82e60357a2a4ef34d5496ffcc306cd6
zsqzsq1993 <zsqzsq1993@yeah.net>
2020-09-07 11:00:51 +0800

    add loading page

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,42 @@
       <my-projects></my-projects>
     </div>
     <div class="col-2-12"></div>
+    <loading ref="loading"></loading>
   </div>
 </template>
 
 <script>
   import MyHeader from 'components/m-header/MyHeader'
   import MyProjects from 'components/m-projects/MyProjects'
+  import Loading from 'components/m-loading/Loading'
+  import { mapGetters } from 'vuex'
 
   export default {
     components: {
       MyHeader,
-      MyProjects
+      MyProjects,
+      Loading
+    },
+
+    computed: {
+      ...mapGetters(['language'])
+    },
+
+    watch: {
+      language (newVal, oldVal) {
+        if (newVal !== oldVal) {
+          this.activateLoading()
+        }
+      }
+    },
+
+    methods: {
+      activateLoading () {
+        this.$refs.loading.show()
+        setTimeout(() => {
+          this.$refs.loading.hide()
+        }, 800)
+      }
     }
   }
 </script>
