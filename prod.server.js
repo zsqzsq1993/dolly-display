bfc0cc12cef2d53915d5df498f265e822fce5f89
zsqzsq1993 <zsqzsq1993@yeah.net>
2020-09-13 20:46:18 +0800

    update QR code

--- prod.server.js
+++ prod.server.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const credentail = {
+  key: fs.readFileSync('/etc/nginx/1_dollylosingweight.today_bundle.crt'),
+  cert: fs.readFileSync('/etc/nginx/2_dollylosingweight.today.key')
+}
 
 const app = express()
 
@@ -6,6 +12,8 @@ const port = 8999
 
 app.use(express.static('./dist'))
 
-app.listen(port, () => {
-  console.log(`listen on localhost:${port}`)
+const httpsServer = https.createServer(credentail, app)
+
+httpsServer.listen(port, () => {
+  console.log(`https://listen on localhost:${port}`)
 })
