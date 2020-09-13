73a7e13bfa47cdb13b09964e269d697176361aff
zsqzsq1993 <zsqzsq1993@yeah.net>
2020-09-13 22:25:00 +0800

    fix favicon.ico bugs

--- vue.config.js
+++ vue.config.js
@@ -18,5 +18,15 @@ module.exports = {
         js: resolve('src/assets/js')
       }
     }
+  },
+
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
   }
 }
